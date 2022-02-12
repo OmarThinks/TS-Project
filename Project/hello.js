01f6c88a02d124787c8955788a9ddb2d7022d908
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-13 00:10:21 +0200

    Void type

--- Project/hello.js
+++ Project/hello.js
@@ -1,4 +1,12 @@
 "use strict";
-function len(x) {
-    return x.length;
-}
+const f1 = () => {
+    return true;
+};
+const f2 = () => true;
+const f3 = function () {
+    return true;
+};
+const v1 = f1();
+const v2 = f2();
+const v3 = f3();
+console.log(typeof v1, v2, v3);
--- Project/hello.ts
+++ Project/hello.ts
@@ -1,8 +1,31 @@
-//function len(s: string): number{ return 1;}
-function len(s: string): number;
-function len(arr: any[]): number;
-function len(x: any) {
-  return x.length;
-}
+type voidFunc = () => void;
+ 
+const f1: voidFunc = () => {
+  return true;
+};
+ 
+const f2: voidFunc = () => true;
+ 
+const f3: voidFunc = function () {
+  return true;
+};
 
 
+
+
+
+
+
+
+
+
+
+const v1 = f1();
+ 
+const v2 = f2();
+ 
+const v3 = f3();
+
+
+
+console.log(typeof v1, v2, v3);
