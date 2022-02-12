de446611772cac4c9064046bd65e148c774e7db9
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-12 21:51:02 +0200

    Changed Type Arguments

--- Project/hello.js
+++ Project/hello.js
@@ -2,3 +2,13 @@
 function map(arr, func) {
     return arr.map(func);
 }
+function longest(a, b) {
+    if (a.length > b.length) {
+        return a;
+    }
+    return b;
+}
+function combine(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+combine([1, 2, 3], ["Hello"]);
--- Project/hello.ts
+++ Project/hello.ts
@@ -6,8 +6,24 @@ function map<Input, Output>(
     }
 
 
+function longest<Type extends {length:number}>(
+    a:Type, b:Type) : Type
+{
+    if(a.length>b.length){return a;}
+    return b;
+}
 
 
+
+
+function combine<Type>(arr1:Type[], arr2:Type[]):Type[]
+{
+    return arr1.concat(arr2);
+}
+
+
+
+combine<string|number>([1,2,3], ["Hello"]);
 
 
 
