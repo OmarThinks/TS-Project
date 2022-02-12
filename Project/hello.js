71a7d1e383fdd59d030f2250cce1f57d7717fc6d
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-12 22:51:02 +0200

    Overridden a function

--- Project/hello.js
+++ Project/hello.js
@@ -1,14 +1,4 @@
 "use strict";
-function map(arr, func) {
-    return arr.map(func);
+function len(x) {
+    return x.length;
 }
-function longest(a, b) {
-    if (a.length > b.length) {
-        return a;
-    }
-    return b;
-}
-function combine(arr1, arr2) {
-    return arr1.concat(arr2);
-}
-combine([1, 2, 3], ["Hello"]);
--- Project/hello.ts
+++ Project/hello.ts
@@ -1,29 +1,8 @@
-
-
-function map<Input, Output>(
-    arr:Input[], func:(arr:Input)=>Output):Output[]{
-        return arr.map(func)
-    }
-
-
-function longest<Type extends {length:number}>(
-    a:Type, b:Type) : Type
-{
-    if(a.length>b.length){return a;}
-    return b;
+//function len(s: string): number{ return 1;}
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
 }
-
-
-
-
-function combine<Type>(arr1:Type[], arr2:Type[]):Type[]
-{
-    return arr1.concat(arr2);
-}
-
-
-
-combine<string|number>([1,2,3], ["Hello"]);
-
 
 
